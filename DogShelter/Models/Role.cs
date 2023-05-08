14cb683e890970e2e30c35c5697f2ae107ebae23
ioanaposea <yoa.pos02@gmail.com>
2023-05-08 19:45:09 +0300

    Added models for BaseEntity, Role and User

---
+++ DogShelter/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogShelter.Models
+{
+    public class Role : BaseEntity
+    {
+        public string Type { get; set; }
+    }
+}
---
+++ DogShelter/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogShelter.Models
+{
+    public class User : BaseEntity
+    {
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
