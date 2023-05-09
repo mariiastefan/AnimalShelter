92f3dd189cecceb458988e9f0c1df9f5363a8574
ioanaposea <yoa.pos02@gmail.com>
2023-05-09 11:43:48 +0300

    Added controller, service and repository for User

---
+++ DogShelter/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using DogShelter.Models;
+
+namespace DogShelter.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
